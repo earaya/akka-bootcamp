60863f02f62d70eddf79d722129db61b9eecf338
Esteban Araya <esteban.araya@gmail.com>
2015-06-13 15:13:14 -0600

    lesson 1.2

---
+++ src/Unit-1/DoThis/Messages/InputError.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTail.Messages
+{
+    internal class InputError
+    {
+        internal string Reason { get; private set; }
+
+        internal InputError(string reason)
+        {
+            Reason = reason;
+        }
+    }
+
+    internal class NullInputError : InputError
+    {
+        internal NullInputError(string reason) : base(reason) { }
+    }
+
+    internal class ValidationError : InputError
+    {
+        internal ValidationError(string reason) : base(reason) { }
+    }
+}
---
+++ src/Unit-1/DoThis/Messages/InputSuccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTail.Messages
+{
+    internal class InputSuccess
+    {
+        internal string Reason { get; private set; }
+
+        internal InputSuccess(string reason)
+        {
+            Reason = reason;
+        }
+    }
+}
